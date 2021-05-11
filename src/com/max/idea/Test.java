ef98ac10b64583f0a3cad5d968db33375019b83a
ipatug1185 <abidarixa@mail.ru>
2021-05-11 22:23:39 +0300

    добавлен новый класс Test, созданы переменные

---
+++ src/com/max/idea/Test.java
@@ -0,0 +1,16 @@
+package com.max.idea;
+
+public class Test {
+    public static void main(String[] args) {
+        double a = 15.5596;
+        byte b = 127;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
+
+
+
+
+
